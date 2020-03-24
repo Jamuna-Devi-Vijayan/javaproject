fb18e5f8d0e496cd2c4768993782d39f9809e717
EC2 Default User <ec2-user@ip-172-31-24-198.us-west-2.compute.internal>
2020-03-24 09:08:53 +0000

    test2

--- api/src/main/java/com/in28minutes/maven/App.java
+++ api/src/main/java/com/in28minutes/maven/App.java
@@ -11,6 +11,7 @@ public class App
         System.out.println( "Hello World Test My Code" );
         System.out.println( "Hello World Changed");
         System.out.println( "Hello World Changed new" );
+        System.out.println( "Hello World Changed after adding build number" );
     }
 
 	public int calculateSomething() {
